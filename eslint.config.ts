b2d273d90e5599b567600ed959d16d2bee769587
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-08-30 11:50:59 -0300

    chore: add eslint and prettier setup

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+  tseslint.configs.recommended,
+]);
