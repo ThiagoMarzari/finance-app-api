1ec7a68f66225c816ec9fdb7a4beba0a73947cb2
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-08-31 17:09:47 -0300

    feat: add CreateUserRepository

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 import 'dotenv/config'
 import express from 'express'
-import { postgresClient } from './db/postgres/helper.js'
 import { env } from './config/env.js'
 
 const app = express()
-
-//Pega tu que a gente manda no body e converte para json
 app.use(express.json())
-
-app.get('/', async (req, res) => {
-  const results = await postgresClient.query('SELECT * FROM users')
-  console.log(req.body)
-  console.log(req.headers)
-  res.status(200).send(results)
-})
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`)
---
+++ src/repositories/create-user.ts
@@ -0,0 +1,19 @@
+import { postgresClient } from '../db/postgres/helper.ts'
+
+interface createUserProps {
+  id: string
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
+
+export class CreateUserRepository {
+  async execute({ id, firstName, lastName, email, password }: createUserProps) {
+    const result = await postgresClient.query(
+      'INSERT INTO users (id ,first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
+      [id, firstName, lastName, email, password],
+    )
+    return result
+  }
+}
