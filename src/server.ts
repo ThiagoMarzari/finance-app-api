f07c6e0c2c3e8e560513cf64d2be054f3ad1db4b
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-08-31 18:24:14 -0300

    feat: implement server setup with user creation endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
-import { env } from './config/env.js'
+import { env } from './config/env.ts'
+import { CreateUserController } from './controllers/create-user-controller.ts'
 
 const app = express()
 app.use(express.json())
+
+app.post('/api/users', new CreateUserController().execute)
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`)
