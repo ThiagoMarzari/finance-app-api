8697fb8c8d1b04e7cb9489531d7a85146e730f20
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-08-31 12:14:12 -0300

    refactor(eslint): update eslint config to use typescript-eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,23 +1,27 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-export default defineConfig([
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    extends: [js.configs.recommended],
     languageOptions: {
       globals: globals.node,
-      parser: tseslint.parser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        tsconfigRootDir: __dirname,
       },
     },
     rules: {
       'no-unused-vars': 'warn',
     },
   },
-  tseslint.configs.recommended,
-])
+)
