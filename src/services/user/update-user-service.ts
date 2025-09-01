a412c13776f82d29fc918800dd0129c6a527c3b1
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-09-01 16:03:43 -0300

    feat: add update user service

---
+++ src/services/user/update-user-service.ts
@@ -0,0 +1,38 @@
+import bcrypt from 'bcryptjs'
+import { EmailAlreadyExists } from '../../errors/user.ts'
+import { GetUserByEmailRepository } from '../../repositories/user/get-user-by-email.ts'
+import { UpdateUserRepository } from '../../repositories/user/update-user-repository.ts'
+
+interface updateUserProps {
+  firstName?: string
+  lastName?: string
+  email?: string
+  password?: string
+}
+
+export class UpdateUserService {
+  async execute(userId: string, updateUser: updateUserProps) {
+    if (updateUser.email) {
+      const getUserByEmailRepository = new GetUserByEmailRepository()
+      const existsUserEmail = await getUserByEmailRepository.execute(
+        updateUser.email,
+      )
+      if (existsUserEmail) {
+        throw new EmailAlreadyExists()
+      }
+    }
+    const user = {
+      ...updateUser,
+    }
+
+    if (updateUser.password) {
+      const hashedPassword = await bcrypt.hash(updateUser.password, 10)
+
+      user.password = hashedPassword
+    }
+
+    const updateUserRepository = new UpdateUserRepository()
+    const updatedUser = await updateUserRepository.execute(userId, user)
+    return updatedUser
+  }
+}
