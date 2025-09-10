68c7d0bb8d2aa70f3ff13287ee9b37076b5cd181
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-09-09 21:59:10 -0300

    feat(prisma): add prisma client and user model migration

---
+++ prisma/migrations/20250910005648_add_user_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
