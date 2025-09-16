97f759dd3bb637bf40098c0f73cf3ed2cb9d7e6b
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-09-16 16:48:56 -0300

    test(controllers): add delete user controller test case

---
+++ tests/controllers/user/delete-user-controller.test.ts
@@ -0,0 +1,46 @@
+import { DeleteUserService } from '../../../src/services'
+import { DeleteUserController } from '../../../src/controllers'
+import { Request, Response } from 'express'
+
+describe('DeleteUserController', () => {
+  let deleteUserController: DeleteUserController
+  let fakeService: { execute: jest.Mock }
+  let request: Partial<Request>
+  let response: Partial<Response>
+
+  const mockDeletedUser = {
+    id: '78b1cbd6-5154-4af3-b2fa-1be9a8bbdd1e',
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'john.doe@example.com',
+    password: 'hashedPassword',
+  }
+
+  beforeEach(() => {
+    request = {
+      params: {
+        id: '78b1cbd6-5154-4af3-b2fa-1be9a8bbdd1e',
+      },
+    }
+    response = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+    const service = { execute: jest.fn() }
+    const sut = new DeleteUserController(
+      service as unknown as DeleteUserService,
+    )
+    deleteUserController = sut
+    fakeService = service
+  })
+
+  it('should return 200 if user is deleted', async () => {
+    //arrange
+    fakeService.execute.mockResolvedValue(mockDeletedUser)
+
+    //act
+    await deleteUserController.execute(request as Request, response as Response)
+    //assert
+    expect(response.status).toHaveBeenCalledWith(200)
+  })
+})
